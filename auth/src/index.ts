fdf9e70af492c84c817539b685d0f5fd7d4fc95b
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-01 10:58:44 +1100

    add ingress services to infra file using ingress=nginx, verify withint broswer that the results is sending

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,9 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there");
+});
 app.listen(3000, () => {
   console.log("listening on port 3000...");
 });
