13a27fdc96dad8686cc738f8711b2b833903749d
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-04 10:52:19 +1100

    standadise throwing error for dberoor and requestvalidationeror as {error[{message: err, field: name of erro}]}

--- auth/middewares/error-handlers.ts
+++ auth/middewares/error-handlers.ts
@@ -1,11 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../src/erros/request-validation-error";
+import { DatabaseConnectionError } from "../src/erros/database-connection-error";
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    // object of errors(array) [errors{}]
+    const formateddErrors = err.errors.map((error) => {
+      return { meesage: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formateddErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
   res.status(400).send({
     message: err.message,
   });
