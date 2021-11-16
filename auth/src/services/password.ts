bbfc6b8bc7f8baa799cb532d54f9fa352abba433
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-16 20:05:07 +1100

    install cookie session into auth direcotry, put middlesware into index.js and trust proxy based on ngrix

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -12,10 +12,8 @@ export class Password {
   }
   // in this case storedPassword is return from toHash
   static async compare(storedPassword: string, suppliedPassword: string) {
-      const [hashedPassword, salt] = storedPassword.split('.');
-      const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-      return buf.toString('hex') === hashedPassword;
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+    return buf.toString("hex") === hashedPassword;
   }
-
-
 }
