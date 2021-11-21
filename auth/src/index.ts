d476c3093067fbefd8dea634b7d20c56cebef7dd
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-21 18:18:43 +1100

    make a scecret object in kuberneties for JWT_KEY and provide access to auth to KWT_KEY using a env within its intiual deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -30,6 +30,10 @@ app.all("*", async (req, res) => {
 });
 app.use(errorHandler);
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to DB using mongoose...");
@@ -39,7 +43,6 @@ const start = async () => {
   app.listen(3000, () => {
     console.log("listening on port 3000...");
   });
-
 };
 
 start();
