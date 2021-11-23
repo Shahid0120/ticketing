d8f214e14e250ca9620cb7585d84caad1468f1ff
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-23 19:35:15 +1100

    make a app.ts file to allow for testing useing super test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,11 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-import jwt from "jsonwebtoken";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handlers";
-import { NotFoundErorr } from "./errors/not-found-error";
-const app = express();
+import { app } from "./app";
 
-app.use(json());
-app.set("trust proxy", true);
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.all("*", async (req, res) => {
-  throw new NotFoundErorr();
-});
-app.use(errorHandler);
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
-  
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to DB using mongoose...");
