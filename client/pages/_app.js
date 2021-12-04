3d1a5696dd57c6c2d4c5bc0bca07c2ffa2ca5590
Shahid0120 <shahid.hussain0120@gmail.com>
2021-12-04 13:41:24 +1100

    continue to fix proble on componentstree getInitialProps problem in client directory

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,24 @@
 import "bootstrap/dist/css/bootstrap.css";
-
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  //   when you console log appContext is shows the componets in Apptree thats
+  // are trying to use getInitsalProps this is finding those compoents
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+
+  return data;
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,17 @@
 import buildClient from "../api/build-client";
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>This is the landing Page.</h1>;
+  return (
+    <div>
+      <h1>This is the landing page</h1>
+      currentUser ? (<h1>You are not signed in!</h1>) : (
+      <h1>You are signed in!</h1>)
+    </div>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get("/api/users/currentuser");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
   return data;
 };
 
