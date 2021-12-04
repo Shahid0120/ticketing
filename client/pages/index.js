df801e93a446cdfbdb576cea9d7100931dc279b5
Shahid0120 <shahid.hussain0120@gmail.com>
2021-12-04 11:23:15 +1100

    customise the landingpage to check for if user is signed in or not using getInitiaalProps, if it is signed make a check if its in the broswer or server, if on the broswer use base url but if it is on the sever send to ingress whcih will sent to the auth service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,30 @@
-export default function App() {
-  return (
-    <>
-      <h1>This is the home page</h1>
-    </>
-  );
-}
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>This is the landing Page.</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  // req.header => where the cookie is localted and hostname 'ticketing.dev'
+  //   the req.header has all the information sent from the licent server to the ingress service/namespace to auth service
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // reqeust madde to http://NAMESPACE.NAMESPACE_SERVICE
+    const { data } = await axios.get(
+      "http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: req.headers,
+        },
+      }
+    );
+  } else {
+    //   we are on the broswer!
+    // base url
+    const { data } = await axios("/api/users/currentuser");
+    return data;
+    // {currentUser : null}
+  }
+};
+
+export default LandingPage;
